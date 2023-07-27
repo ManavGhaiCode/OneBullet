af2e7da9d0602646a218880fbc4f83fcd841e18e
ManavGhaiCode <manavghai12october@gmail.com>
2023-07-27 15:13:23 +0530

    add moving Enemy

--- Scripts/Enemies/Enemy.cs
+++ Scripts/Enemies/Enemy.cs
@@ -3,6 +3,12 @@ using UnityEngine;
 public class Enemy : MonoBehaviour {
     public int health = 5;
 
+    [HideInInspector] public Rigidbody2D rb;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     public void TakeDamage(int damage) {
         health -= damage;
 
---
+++ Scripts/Enemies/MovingEnemy.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class MovingEnemy : Enemy {
+    public Transform[] movePoints;
+    private int CIndex = 0;
+
+    private void Update() {
+        if (Vector2.Distance(rb.position, movePoints[CIndex].position) < .05f) {
+            if (CIndex == 0) {
+                CIndex = 1;;
+            } else {
+                CIndex = 0;
+            }
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, movePoints[CIndex].position, Time.deltaTime);
+    }
+}
