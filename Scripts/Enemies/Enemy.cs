17eab25269eb32fd73a55a2e5026c2b3cf771045
ManavGhaiCode <manavghai12october@gmail.com>
2023-07-27 19:45:13 +0530

    implement Enemy death effect

--- Scripts/Enemies/Enemy.cs
+++ Scripts/Enemies/Enemy.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour {
     public int health = 5;
+    public GameObject EnemyDeathEffect;
 
     [HideInInspector] public Rigidbody2D rb;
 
@@ -13,6 +14,7 @@ public class Enemy : MonoBehaviour {
         health -= damage;
 
         if (health >= 0) {
+            Instantiate(EnemyDeathEffect);
             Destroy(gameObject);
         }
     }
