4639207b61b7cd01fa7ad8954676ee14f41aadee
ManavGhaiCode <manavghai12october@gmail.com>
2023-07-27 13:28:01 +0530

    add gun

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -2,9 +2,13 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
     public float speed = 5f;
+    public float BulletSpeed = 2f;
 
     private float moveInput = 0;
     private Rigidbody2D rb;
+
+    [SerializeField] private GameObject BulletPerfab;
+    [SerializeField] private Transform FirePoint;
 
     private void Start() {
         rb = GetComponent<Rigidbody2D>();
@@ -17,5 +21,10 @@ public class Player : MonoBehaviour {
     private void FixedUpdate() {
         float Force = moveInput * speed * Time.fixedDeltaTime;
         rb.MovePosition(rb.position + new Vector2 (Force, 0));
+
+        if (Input.GetKey(KeyCode.Space)) {
+            GameObject Bullet = Instantiate(BulletPerfab, FirePoint.position, FirePoint.rotation);
+            Bullet.GetComponent<Rigidbody2D>().AddForce(Vector2.up * BulletSpeed, ForceMode2D.Impulse);
+        }
     }
 }
